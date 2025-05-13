955f05ccd633ea830ef2e53a17183384d115d19c
r3lllax <turkevicaleksandr@gmail.com>
2025-05-13 18:34:40 +0300

    minor edits

--- src/components/ErrorDescription.vue
+++ src/components/ErrorDescription.vue
@@ -10,12 +10,13 @@
     if(error!==undefined){
         error = error.map(function (elem){
           let index = elem.indexOf('field')
-          elem = elem.slice(index,elem.length)
-          elem = elem.replace("f","F")
+          if(index>=0){
+            elem = elem.slice(index,elem.length)
+            elem = elem.replace("f","F")
+          }
           return elem
       })
     }
-    console.log(error)
     return error
   }
 
@@ -23,6 +24,6 @@
 
 <template>
   <div class="invalid-description" v-if="error">
-    {{ParseError(error).join(' ')}}
+    {{ParseError(error).join(', ')}}
   </div>
 </template>
